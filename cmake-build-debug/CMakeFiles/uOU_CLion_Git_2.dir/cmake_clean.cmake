5a30aa2eb86d8499b96b7618be6d9bdc0d1ca73f
Xardas25 <goszczynski.jakub@gmail.com>
2022-01-23 22:04:44 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/uOU_CLion_Git_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uOU_CLion_Git_2.dir/main.cpp.obj"
+  "uOU_CLion_Git_2.exe"
+  "uOU_CLion_Git_2.exe.manifest"
+  "uOU_CLion_Git_2.lib"
+  "uOU_CLion_Git_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uOU_CLion_Git_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
